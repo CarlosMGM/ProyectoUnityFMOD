ed067752d29396c11fc05312b3befd9e512a7610
AlansonUCM <Alanson@ucm.es>
2020-01-16 22:34:26 +0100

    Boton basico,trigger y colisiones metidas

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour
+{
+    bool trigger = false;
+    public Text texto;
+    public bool pressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressed = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                pressed = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+                pressed = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        texto.text = "Pulsa la E para interactuar";
+        trigger = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        texto.text = "";
+        trigger = false;
+        pressed = false;
+    }
+}
