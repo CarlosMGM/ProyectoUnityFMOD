8ba96fc397d3032d8da1914c17046af03d4469e9
AlansonUCM <Alanson@ucm.es>
2020-01-20 18:40:14 +0100

    Terminado laberinto,arreglado timing de unos sonidos
    Goteras de decoracion añadidas

---
+++ Assets/Scripts/ReproductordeSonido.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproductordeSonido : MonoBehaviour
+{
+    public FMODLoader.SOUNDS sonido;
+    public bool pulsarBoton;
+    FMODSoundEmitter soundEmitter;
+    Trigger triger;
+    bool pulsado = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x=Random.value;
+        soundEmitter = gameObject.GetComponent<FMODSoundEmitter>();
+        if(pulsarBoton)
+        triger = gameObject.GetComponent<Trigger>();
+        if (!pulsarBoton)
+            Invoke("play",x+0.1f*2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pulsarBoton)
+        {
+            if (triger.pressed && !pulsado)
+            {
+                play();
+                pulsado = true;
+            }
+            else if (!triger.pressed)
+            {
+                pulsado = false;
+            }
+        }
+
+    }
+    void play()
+    {
+        soundEmitter.playSound(sonido, true);
+    }
+}
